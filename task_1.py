7d4a3c85f24b78abd4a2bcf5386056db218f6b40
Booharin <booharin@bk.ru>
2019-12-28 22:19:01 +0300

    Updated task_1

---
+++ task_1.py
@@ -0,0 +1,13 @@
+my_list = [
+    2,
+    57,
+    True,
+    55.5,
+    'Python',
+    {'key_1': 2, 'key_2': 5},
+    (4, 8, 193),
+    {75, 56, 30}
+]
+
+for _ in my_list:
+    print(f'Type of {_} is {type(_)}')
